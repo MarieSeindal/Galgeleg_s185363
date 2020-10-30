82becc826697146a5a0d2093e0a8ad12e1c9d962
Marie <S185363@student.dtu.dk>
2020-10-30 14:39:41 +0100

    Progress. Har hentet dele fra galgelogik og android elementer, samt er begyndt at dele logikken op

---
+++ app/src/main/java/com/example/galgeleg_s185363/Opdater.java
@@ -0,0 +1,19 @@
+package com.example.galgeleg_s185363;
+
+public class Opdater {
+//todo
+//    private void opdaterSynligtOrd() {
+//        synligtOrd = "";
+//        spilletErVundet = true;
+//        for (int n = 0; n < ordet.length(); n++) {
+//            String bogstav = ordet.substring(n, n + 1);
+//            if (brugteBogstaver.contains(bogstav)) {
+//                synligtOrd = synligtOrd + bogstav;
+//            } else {
+//                synligtOrd = synligtOrd + "*";
+//                spilletErVundet = false;
+//            }
+//        }
+//    }
+
+}

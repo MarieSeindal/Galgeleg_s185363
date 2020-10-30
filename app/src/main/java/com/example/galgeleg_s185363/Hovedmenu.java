6ce41571607a16bf6bcaf4905d1510d0f6f5a536
Marie <S185363@student.dtu.dk>
2020-10-30 19:17:57 +0100

    Tilføjet original logik fra galgeleg, og begyndt implementering af indstillinger

--- app/src/main/java/com/example/galgeleg_s185363/Hovedmenu.java
+++ app/src/main/java/com/example/galgeleg_s185363/Hovedmenu.java
@@ -1,5 +1,6 @@
 package com.example.galgeleg_s185363;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Hovedmenu extends AppCompatActivity implements View.OnClickListener {
 
-    Button spilKnap, indstillingKnap, lukKnap;
+    Button spilKnap, indstillingKnap, lukKnap, highscoreKnap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,26 +18,39 @@ public class Hovedmenu extends AppCompatActivity implements View.OnClickListener
         setContentView(R.layout.hovemenu);
 
         spilKnap = findViewById(R.id.spilKnap);
-        spilKnap.setText("Spil");
+        spilKnap.setText("Spils");
 
         indstillingKnap = findViewById(R.id.indstillingKnap);
         indstillingKnap.setText("Indstillinger");
+
+        highscoreKnap = findViewById(R.id.highscore);
+        highscoreKnap.setText("Highscore");
 
         lukKnap = findViewById(R.id.lukKnap);
         lukKnap.setText("Luk");
 
         spilKnap.setOnClickListener(this);
         indstillingKnap.setOnClickListener(this);
+        highscoreKnap.setOnClickListener(this);
         lukKnap.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         if (v == spilKnap){
-
+            Intent i = new Intent(this, Spillet_akt.class);
+            //i.putExtra("velkomst", "\n\nHalløj fra Hovedmenu_akt!\n");
+            startActivity(i);
         } else if(v == indstillingKnap){
+            Intent i = new Intent(this, Indstillinger.class);
+            //i.putExtra("velkomst", "\n\nHalløj fra Hovedmenu_akt!\n");
+            startActivity(i);
+
+        } else if(v == highscoreKnap){
+
 
         } else if(v == lukKnap){
+
 
         }
 
---
+++ app/src/main/java/com/example/galgeleg_s185363/Spillet_akt.java
@@ -0,0 +1,85 @@
+package com.example.galgeleg_s185363;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Spillet_akt extends AppCompatActivity implements View.OnClickListener {
+
+    Galgelogik logik = new Galgelogik();
+    private TextView info;
+    private Button spilKnap;
+    private EditText et;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Programmatisk layout
+        TableLayout tl = new TableLayout(this);
+
+        info = new TextView(this);
+        info.setText("Velkommen til mit fantastiske spil." +
+                "\nDu skal gætte dette ord: "+logik.getSynligtOrd() +
+                "\nSkriv et bogstav herunder og tryk 'Spil'.\n");
+/*
+  Hovedmenu_akt startede denne aktivitet med
+      Intent i = new Intent(this, Spillet_akt.class);
+      i.putExtra("velkomst", "\n\nHalløj fra Hovedmenu_akt!\n");
+      startActivity(i);
+  derfor vil velkomst få værdien 'Halløj fra Hovedmenu_akt!' nedenfor
+ */
+
+        String velkomst = getIntent().getStringExtra("velkomst");
+        if (velkomst!=null) info.append(velkomst);
+        tl.addView(info);
+
+        et = new EditText(this);
+        et.setHint("Skriv et bogstav her.");
+        tl.addView(et);
+
+        spilKnap = new Button(this);
+        spilKnap.setText("Spil");
+        spilKnap.setCompoundDrawablesWithIntrinsicBounds(android.R.drawable.ic_media_play, 0, 0, 0);
+        tl.addView(spilKnap);
+
+        spilKnap.setOnClickListener(this);
+
+        setContentView(tl);
+        logik.logStatus(); // Så vi kan se det rigtige ord i loggen
+    }
+
+    @Override
+    public void onClick(View v) {
+        String bogstav = et.getText().toString();
+        if (bogstav.length() != 1) {
+            et.setError("Skriv præcis ét bogstav");
+            return;
+        }
+        logik.gætBogstav(bogstav);
+        et.setText("");
+        et.setError(null);
+        spilKnap.animate().rotationBy(3*360).setInterpolator(new DecelerateInterpolator());
+//      spilKnap.animate().translationYBy(30).setInterpolator(new BounceInterpolator());
+        opdaterSkærm();
+    }
+
+
+    private void opdaterSkærm() {
+        info.setText("Gæt ordet: " + logik.getSynligtOrd());
+        info.append("\n\nDu har " + logik.getAntalForkerteBogstaver() + " forkerte:" + logik.getBrugteBogstaver());
+
+        if (logik.erSpilletVundet()) {
+            info.append("\nDu har vundet");
+        }
+        if (logik.erSpilletTabt()) {
+            info.setText("Du har tabt, ordet var : " + logik.getOrdet());
+        }
+    }
+}
